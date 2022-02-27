9fc5fbd2d04429c121ff1e1ecb4c5b501f3c3152
Kevin S. Miller <ksmiller99@gmail.com>
2022-02-27 14:27:10 -0500

    added subsystem stubs

---
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * Feeds the cargo from Indexer to Shooter
+ */
+public class Feeder extends SubsystemBase {
+  /** Creates a new Feeder. */
+  public Feeder() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
